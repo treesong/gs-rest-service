00d4389921dbb73d49d09e330bf8323276ac05ca
ruiqi.zss <ruiqi.zss@alibaba-inc.com>
2020-09-18 14:42:03 +0800

    add ping-pong

--- complete/src/main/java/com/example/restservice/GreetingController.java
+++ complete/src/main/java/com/example/restservice/GreetingController.java
@@ -12,6 +12,11 @@ public class GreetingController {
 	private static final String template = "Hello, %s!";
 	private final AtomicLong counter = new AtomicLong();
 
+	@GetMapping("/ping")
+	public String ping() {
+		return "pong";
+	}
+
 	@GetMapping("/greeting")
 	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
 		return new Greeting(counter.incrementAndGet(), String.format(template, name));
