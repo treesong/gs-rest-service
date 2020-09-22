7cc5ca2b7a19c8139eb19f7332c2cd53af4c5674
ruiqi.zss <ruiqi.zss@alibaba-inc.com>
2020-09-22 11:58:32 +0800

    update message

--- complete/src/main/java/com/example/restservice/GreetingController.java
+++ complete/src/main/java/com/example/restservice/GreetingController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class GreetingController {
 
-	private static final String template = "Hello, %s!";
+	private static final String template = "你好, %s!";
 	private final AtomicLong counter = new AtomicLong();
 
 	@GetMapping("/ping")
@@ -18,7 +18,7 @@ public class GreetingController {
 	}
 
 	@GetMapping("/greeting")
-	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+	public Greeting greeting(@RequestParam(value = "name", defaultValue = "云计算") String name) {
 		return new Greeting(counter.incrementAndGet(), String.format(template, name));
 	}
 }
